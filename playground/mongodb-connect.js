6552d0da0ab6cfc1587990725bcb5d4ea04a2b15
jamesferry11 <jamesferry11@gmail.com>
2017-02-27 00:48:50 -0700

    update on playground file

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -17,16 +18,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'James',
-        age: 32,
-        location: 'Salt Lake City'
-    }, (err, result) => {
-       if (err) {
-           return console.log('Unable to insert user', err);
-       }
-       console.log(result.ops);
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'James',
+    //     age: 32,
+    //     location: 'Salt Lake City'
+    // }, (err, result) => {
+    //    if (err) {
+    //        return console.log('Unable to insert user', err);
+    //    }
+    //    console.log(result.ops[0]._id.getTimestamp());
+    // });
 
       db.close();
 
