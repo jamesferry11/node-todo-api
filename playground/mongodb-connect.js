ce4879ce25960657ef2b3006a4d9420114a075ea
jamesferry11 <jamesferry11@gmail.com>
2017-02-27 23:24:10 -0700

    updated the gitignore. added mongodb search option

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,27 +7,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert todo', err);
-    //     }
-    //
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
 
-    // db.collection('Users').insertOne({
-    //     name: 'James',
-    //     age: 32,
-    //     location: 'Salt Lake City'
-    // }, (err, result) => {
-    //    if (err) {
-    //        return console.log('Unable to insert user', err);
-    //    }
-    //    console.log(result.ops[0]._id.getTimestamp());
-    // });
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.collection('Users').insertOne({
+        name: 'James',
+        age: 32,
+        location: 'Salt Lake City'
+    }, (err, result) => {
+       if (err) {
+           return console.log('Unable to insert user', err);
+       }
+       console.log(result.ops[0]._id.getTimestamp());
+    });
 
       db.close();
 
